028876ec8613c81c801c36bc7a1aa8835c7b7ef0
devjeremyjake <devjeremyjake@gmail.com>
2020-10-09 10:00:29 +0100

    latest commit

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Container, Button } from '../../globalStyles';
+import { Link } from 'react-router-dom';
+import {
+	InfoSec,
+	InfoRow,
+	InfoColumn,
+	TextWrapper,
+	TopLine,
+	Heading,
+	Subtitle,
+	ImgWrapper,
+	Img,
+} from './infoSection.elements';
+
+const InfoSection = ({
+	primary,
+	lightBg,
+	imgStart,
+	lightTopLine,
+	lightText,
+	lightTextDesc,
+	topLine,
+	headline,
+	description,
+	buttonLabel,
+	img,
+	alt,
+	start,
+}) => {
+	return (
+		<>
+			<InfoSec lightBg={lightBg}>
+				<Container>
+					<InfoRow imgStart={imgStart}>
+						<InfoColumn>
+							<TextWrapper>
+								<TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+								<Heading lightText={lightText}>{headline}</Heading>
+								<Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+								<Link to="/sign-up">
+									<Button big font-big primary={primary}>
+										{buttonLabel}
+									</Button>
+								</Link>
+							</TextWrapper>
+						</InfoColumn>
+						<InfoColumn>
+							<ImgWrapper start={start}>
+								<Img src={img} alt={alt} />
+							</ImgWrapper>
+						</InfoColumn>
+					</InfoRow>
+				</Container>
+			</InfoSec>
+		</>
+	);
+};
+
+export default InfoSection;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,35 @@
+export const homeObjOne = {
+	lightBg: false,
+	primary: true,
+	lightBg: false,
+	imgStart: '',
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: 'Marketing Agency',
+	headline: 'Lead Generation Specialist for Online Business',
+	description:
+		'We help Business Owenrs increase their revenue. Our Team of unique specialist cna help you achieve your Business goals.',
+	buttonLabel: 'Get Started',
+	img: require('../../images/svg-1.svg'),
+	alt: 'image',
+	start: '',
+};
+
+export const homeObjTwo = {
+	lightBg: false,
+	primary: true,
+	lightBg: false,
+	imgStart: '',
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: 'Marketing Agency',
+	headline: 'Lead Generation Specialist for Online Business',
+	description:
+		'We help Business Owenrs increase their revenue. Our Team of unique specialist cna help you achieve your Business goals.',
+	buttonLabel: 'Get Started',
+	img: require('../../images/profile.jpg'),
+	alt: 'image',
+	start: '',
+};
